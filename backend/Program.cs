87350e32cce264267b2ea0ddaaeed5b44891af1b
DeJa Barclay <barclay.deja@gmail.com>
2026-09-09 12:33:28 -0500

    chore: enable production CORS for Amplify

--- backend/Program.cs
+++ backend/Program.cs
@@ -18,7 +18,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
         policy.WithOrigins(
                 "http://localhost:3000",
-                "http://127.0.0.1:3000")
+                "http://127.0.0.1:3000",
+                "https://master.d3uj4k8iqldq7q.amplifyapp.com"
+                )
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
